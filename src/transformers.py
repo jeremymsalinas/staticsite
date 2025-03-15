0e8c38e2428929f9030af33736c10860473da6e0
Jeremy Salinas <jeremymsalinas@gmail.com>
2025-03-15 11:37:00 -0500

    add site source

--- src/transformers.py
+++ src/transformers.py
@@ -188,7 +188,7 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, bas
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
-        if os.path.isfile(from_path):
+        if os.path.isfile(from_path) and Path(from_path).suffix == ".md":
             dest_path = Path(dest_path).with_suffix(".html")
             generate_page(from_path, template_path, dest_path, basepath)
         else:
